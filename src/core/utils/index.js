ffaa7cd7824a73d0d194f15424fe3fb950014a73
sijju <karthik.msv1234@gmail.com>
2023-09-04 00:43:20 +0530

    minor fixes

--- src/core/utils/index.js
+++ src/core/utils/index.js
@@ -1,2 +1,2 @@
 const production = process.env.NODE_ENV === 'production'
-export const SITE_URL = production ? 'prod_url' : 'http://localhost:3000'
+export const SITE_URL = production ? 'https://sass-website-tau.vercel.app/' : 'http://localhost:3000'
